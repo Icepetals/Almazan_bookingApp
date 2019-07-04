10b85d910cdf8fcc69fe2a1c7a4aa8a33fed71ab
Angelica <jicaalmazan21@gmail.com>
2019-07-04 14:30:54 +0800

    added modals

--- profile.js
+++ profile.js
@@ -2,9 +2,3 @@ function togglemenu(){
     document.getElementById('sideBar').classList.toggle('active');
 }
 
-function checkInput(){
-    if(document.getElementById('username') && document.getElementById('password')===""){
-        alert("please enter a valid input");
-    }
-    return;
-}
